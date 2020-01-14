531ce41f1a7e873a7787a3b836bd0a8c4ec062ca
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 22:40:04 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,9 @@
-print("hello")
-print("world")
-print("hello")
+def grow_rectangle(rect, dwidth, dheight):
+    rect.width += dwidth
+    rect.height += dheight
+    
+>>> box.width, box.height
+(150.0, 300.0)
+>>> grow_rectangle(box, 50, 100)
+>>box.width, box.height
+(200.0, 400.0)
